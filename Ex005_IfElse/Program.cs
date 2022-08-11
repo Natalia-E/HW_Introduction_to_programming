7065e79a0b5645212cfe43f2f94644695179b7d6
Natalia Efremenko <natalia.efremenko21@gmail.com>
2022-08-11 12:13:24 +0300

    Ex005_If_Else added

---
+++ Ex005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "наташа")
+{
+    Console.WriteLine("Урa, это же Наташа!");
+}
+else 
+{Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+}
